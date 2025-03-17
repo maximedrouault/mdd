cb4e5d884a79ea3fe6fe3ac90cc6e179a0494d9e
Maxime DROUAULT <maximedrouault@gmail.com>
2025-03-17 13:10:46 +0100

    first commit

---
+++ back/src/test/java/org/mdd/mddapi/MddApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mdd.mddapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MddApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
