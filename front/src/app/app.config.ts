23af993367821dc5d42040ca3db8b8713065fd7f
Maxime DROUAULT <maximedrouault@gmail.com>
2025-03-20 14:55:38 +0100

    feat: enhance TopicListComponent with PrimeNG card layout and global font styling

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -3,11 +3,23 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+import {MyPreset} from '../themePreset'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: 'light',
+        }
+      }
+    })
   ],
 };
--- front/src/app/features/topics/components/topic-list/topic-list.component.ts
+++ front/src/app/features/topics/components/topic-list/topic-list.component.ts
@@ -2,14 +2,16 @@ import {Component, OnInit} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {Topic} from '../../interfaces/topic.interface';
 import {TopicsService} from '../../services/topics.service';
+import {Card} from 'primeng/card';
 import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-topic-list',
   imports: [
-    NgForOf,
+    Card,
     AsyncPipe,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './topic-list.component.html',
   styleUrl: './topic-list.component.scss'
@@ -18,7 +20,7 @@ export class TopicListComponent implements OnInit{
 
   topics$: Observable<Topic[]> = of();
 
-  constructor(private topicService: TopicsService) {}
+  constructor(private readonly topicService: TopicsService) {}
 
   ngOnInit(): void {
     this.topics$ = this.topicService.getAllTopic();
