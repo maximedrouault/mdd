d949b146be19cb60cf59bbe202ba628dcbb014c2
maximedrouault <maximedrouault@gmail.com>
2025-03-24 16:00:50 +0100

    feat: update topic components to handle undefined states and improve subscription logic

--- front/src/app/features/topics/components/topic-list/topic-list.component.ts
+++ front/src/app/features/topics/components/topic-list/topic-list.component.ts
@@ -16,24 +16,21 @@ import {TopicDetailsComponent} from '../topic-details/topic-details.component';
 })
 export class TopicListComponent implements OnInit{
 
-  allTopics$: Observable<Topic[]> = of();
-  userId: number = 1; // TODO: get the user id from the logged in user when the authentication is implemented
-  subscribedTopics$: Observable<Topic[]> = of();
+  allTopics$: Observable<Topic[] | undefined> = of(undefined);
+  userId: number = 3; // TODO: get the user id from the logged in user when the authentication is implemented
+  subscribedTopicIds: Set<number> | null = null;
 
   constructor(private readonly topicService: TopicsService) {}
 
   ngOnInit(): void {
     this.allTopics$ = this.topicService.getAllTopics();
-    this.subscribedTopics$ = this.topicService.getSubscribedTopics(this.userId);
+    this.topicService.getSubscribedTopics(this.userId).subscribe(
+      (topics: Topic[]): void => {
+      this.subscribedTopicIds = new Set(topics.map((topic: Topic): number => topic.id));
+    });
   }
 
-  isSubscribed(topicId: number): boolean {
-    let isSubscribed: boolean = false;
-
-    this.subscribedTopics$.subscribe((subscribedTopics: Topic[]): void => {
-      isSubscribed = subscribedTopics.some((subscribedTopic: Topic): boolean => subscribedTopic.id === topicId);
-    });
-
-    return isSubscribed;
-  };
+  isSubscribed(topicId: number): boolean | undefined {
+    return this.subscribedTopicIds === null ? undefined : this.subscribedTopicIds.has(topicId);
+  }
 }
--- front/src/app/features/topics/services/topics.service.ts
+++ front/src/app/features/topics/services/topics.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
 import {Topic} from '../interfaces/topic.interface';
 import {environment} from '../../../../environments/environment.development';
 
@@ -16,17 +16,6 @@ export class TopicsService {
   }
 
   public getSubscribedTopics(userId: number): Observable<Topic[]> {
-    return of([
-      {
-        id: 1,
-        name: 'Subscribed Topic 1',
-        description: 'This is a subscribed topic'
-      },
-      {
-        id: 5,
-        name: 'Subscribed Topic 2',
-        description: 'This is a subscribed topic'
-      }
-    ]);
+    return this.http.get<Topic[]>(`${environment.apiUrl}/user/${userId}/subscribed-topics`);
   }
 }
