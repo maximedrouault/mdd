fe74d2458000e58896e297f5d184bbf154fb53e3
maximedrouault <maximedrouault@gmail.com>
2025-03-27 11:41:54 +0100

    feat: implement subscribed posts display in user post list component

--- front/src/app/features/users/components/user-post-list/user-post-list/user-post-list.component.ts
+++ front/src/app/features/users/components/user-post-list/user-post-list/user-post-list.component.ts
@@ -2,23 +2,27 @@ import {Component, OnInit} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {Post} from '../../../../posts/interfaces/post.interface';
 import {UsersService} from '../../../service/users.service';
+import {PostDetailsComponent} from '../../../../posts/components/post-details/post-details/post-details.component';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-user-post-list',
-  imports: [],
+  imports: [
+    PostDetailsComponent,
+    AsyncPipe
+  ],
   templateUrl: './user-post-list.component.html',
   styleUrl: './user-post-list.component.scss'
 })
 export class UserPostListComponent implements OnInit {
 
-  posts$: Observable<Post[]> = of();
+  subscribedPosts$: Observable<Post[]> = of();
   userId: number = 3; // TODO: get the user id from the logged in user when the authentication is implemented
 
   constructor(private readonly usersService: UsersService) {}
 
-  ngOnInit(): void {
-    this.posts$ = this.usersService.getSubscribedPosts(this.userId);
 
-    this.posts$.subscribe(console.log)
+  ngOnInit(): void {
+    this.subscribedPosts$ = this.usersService.getSubscribedPosts(this.userId);
   };
 }
