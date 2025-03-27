9e3a9d299f9edf007b547ac6fb5b454746ea127b
maximedrouault <maximedrouault@gmail.com>
2025-03-27 15:40:36 +0100

    feat: add sorting functionality to user post list and enhance UI with controls

--- front/src/app/features/users/components/user-post-list/user-post-list.component.ts
+++ front/src/app/features/users/components/user-post-list/user-post-list.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable, of} from 'rxjs';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {map, Observable, of} from 'rxjs';
 import {Post} from '../../../posts/interfaces/post.interface';
 import {UsersService} from '../../service/users.service';
 import {PostDetailsComponent} from '../../../posts/components/post-details/post-details.component';
@@ -14,15 +14,32 @@ import {AsyncPipe} from '@angular/common';
   templateUrl: './user-post-list.component.html',
   styleUrl: './user-post-list.component.scss'
 })
-export class UserPostListComponent implements OnInit {
+export class UserPostListComponent implements OnInit, OnChanges {
 
   subscribedPosts$: Observable<Post[]> = of();
   userId: number = 3; // TODO: get the user id from the logged in user when the authentication is implemented
+  @Input() sortOrderDesc!: boolean;
 
   constructor(private readonly usersService: UsersService) {}
 
 
   ngOnInit(): void {
     this.subscribedPosts$ = this.usersService.getSubscribedPosts(this.userId);
+    this.sortSubscribedPosts();
   };
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['sortOrderDesc']) {
+      this.sortSubscribedPosts();
+    }
+  }
+
+  private sortSubscribedPosts(): void {
+    this.subscribedPosts$ = this.subscribedPosts$.pipe(
+      map(posts => posts.sort((a, b) => this.sortOrderDesc
+        ? new Date(b.creationDate).getTime() - new Date(a.creationDate).getTime()
+        : new Date(a.creationDate).getTime() - new Date(b.creationDate).getTime()
+      ))
+    );
+  }
 }
--- front/src/app/pages/user-post-feed/components/user-post-feed.component.ts
+++ front/src/app/pages/user-post-feed/components/user-post-feed.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {UserPostListComponent} from '../../../features/users/components/user-post-list/user-post-list.component';
+import {Button} from 'primeng/button';
+import {ToggleButton} from 'primeng/togglebutton';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-user-post-feed',
   imports: [
-    UserPostListComponent
+    UserPostListComponent,
+    Button,
+    ToggleButton,
+    FormsModule
   ],
   templateUrl: './user-post-feed.component.html',
   styleUrl: './user-post-feed.component.scss'
 })
 export class UserPostFeedComponent {
 
+  sortOrderDesc: boolean = true;
 }
