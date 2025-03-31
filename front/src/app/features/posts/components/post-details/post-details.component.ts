371778f9fb2a3523f8b0bcf382853d776390e032
maximedrouault <maximedrouault@gmail.com>
2025-03-31 14:44:38 +0200

    feat: implement post details retrieval and display in PostDetailsComponent

--- front/src/app/features/posts/components/post-details/post-details.component.ts
+++ front/src/app/features/posts/components/post-details/post-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {PostDetails} from '../../interfaces/postDetails.interface';
+import {PostsService} from '../../services/posts.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-post-details',
@@ -6,6 +10,17 @@ import { Component } from '@angular/core';
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.scss'
 })
-export class PostDetailsComponent {
+export class PostDetailsComponent implements OnInit {
 
+  constructor(private readonly postService: PostsService,
+              private activatedRoute: ActivatedRoute) {}
+
+  postId!: number;
+  postDetails$: Observable<PostDetails> = of();
+
+  ngOnInit(): void {
+    this.postId = this.activatedRoute.snapshot.params['id'];
+
+    this.postDetails$ = this.postService.getPostDetails(this.postId);
+  }
 }
---
+++ front/src/app/features/posts/interfaces/postDetails.interface.ts
@@ -0,0 +1,10 @@
+export interface PostDetails {
+
+  id: number;
+  title: string;
+  creationDate: Date;
+  authorName: string;
+  topicName: string;
+  content: string;
+
+}
