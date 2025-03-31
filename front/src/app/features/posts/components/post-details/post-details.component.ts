6e4f89703578492d509c90974f389e8e82613de5
maximedrouault <maximedrouault@gmail.com>
2025-03-31 10:19:08 +0200

    feat: apply title case formatting to author names and topic headers

--- front/src/app/features/posts/components/post-details/post-details.component.ts
+++ front/src/app/features/posts/components/post-details/post-details.component.ts
@@ -2,14 +2,15 @@ import {Component, Input} from '@angular/core';
 import {Card} from 'primeng/card';
 import {Post} from '../../interfaces/post.interface';
 import {PrimeTemplate} from 'primeng/api';
-import {DatePipe} from '@angular/common';
+import {DatePipe, TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-post-details',
   imports: [
     Card,
     PrimeTemplate,
-    DatePipe
+    DatePipe,
+    TitleCasePipe
   ],
   templateUrl: './post-details.component.html',
   styleUrl: './post-details.component.scss'
--- front/src/app/features/topics/components/topic-details/topic-details.component.ts
+++ front/src/app/features/topics/components/topic-details/topic-details.component.ts
@@ -3,13 +3,15 @@ import {Topic} from '../../interfaces/topic.interface';
 import {Card} from 'primeng/card';
 import {Button} from 'primeng/button';
 import {PrimeTemplate} from 'primeng/api';
+import {TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-topic-details',
   imports: [
     Card,
     Button,
-    PrimeTemplate
+    PrimeTemplate,
+    TitleCasePipe
   ],
   templateUrl: './topic-details.component.html',
   styleUrl: './topic-details.component.scss'
