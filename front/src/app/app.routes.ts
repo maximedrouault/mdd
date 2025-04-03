9d87d44834ea577c6e5ade32e0d181679730f6e3
Maxime DROUAULT <maximedrouault@gmail.com>
2025-04-03 14:35:00 +0200

    feat: add navigation to post form and implement post creation button in user posts feed

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {AllTopicsListComponent} from './features/topics/components/all-topics-li
 import {UserProfilComponent} from './layouts/user-profil/components/user-profil.component';
 import {UserPostsFeedComponent} from './layouts/user-posts-feed/components/user-posts-feed.component';
 import {PostDetailsPageComponent} from './layouts/post-details-page/post-details-page.component';
+import {PostFormComponent} from './features/posts/components/post-form/post-form.component';
 
 export const routes: Routes = [
   { path: 'user-profile', component: UserProfilComponent },
   { path: 'all-topics-list', component: AllTopicsListComponent },
   { path: 'user-posts-feed', component: UserPostsFeedComponent },
   { path: 'post-details/:id' , component: PostDetailsPageComponent },
+  { path: 'post-form', component: PostFormComponent },
   { path: '**', redirectTo: '' }
 ];
