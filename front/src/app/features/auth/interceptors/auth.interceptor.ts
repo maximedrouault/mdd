0318752c34638daa0e4fbe71f02d6230e57b2597
maximedrouault <maximedrouault@gmail.com>
2025-04-07 16:18:22 +0200

    feat: update auth interceptor to include type annotations and improve token handling

--- front/src/app/features/auth/interceptors/auth.interceptor.ts
+++ front/src/app/features/auth/interceptors/auth.interceptor.ts
@@ -1,13 +1,14 @@
-import {HttpInterceptorFn} from '@angular/common/http';
+import {HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('token');
+export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
+  const token: string | null = localStorage.getItem('token');
 
   if (!token) {
     return next(req);
   }
 
-  const clonedRequest = req.clone({
+  const clonedRequest: HttpRequest<unknown> = req.clone({
     setHeaders: { Authorization: `Bearer ${token}`}
   });
 
