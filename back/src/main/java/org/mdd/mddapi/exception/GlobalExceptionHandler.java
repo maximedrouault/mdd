126abe0dcf4d15a75e0c1de32c7d455438774cab
maximedrouault <maximedrouault@gmail.com>
2025-04-07 12:30:58 +0200

    feat: implement CustomUserDetailsService for user authentication and enhance global exception handling

--- back/src/main/java/org/mdd/mddapi/exception/GlobalExceptionHandler.java
+++ back/src/main/java/org/mdd/mddapi/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package org.mdd.mddapi.exception;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -52,6 +54,13 @@ public class GlobalExceptionHandler {
         log.warn(exception.getMessage());
 
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException exception) {
+        log.warn(exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @ExceptionHandler(Exception.class)
---
+++ back/src/main/java/org/mdd/mddapi/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package org.mdd.mddapi.service;
+
+import lombok.RequiredArgsConstructor;
+import org.mdd.mddapi.entity.User;
+import org.mdd.mddapi.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByEmailOrUsername(username, username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .build();
+    }
+}
