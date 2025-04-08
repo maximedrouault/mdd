6912559f22b2b6bc71483280b64f8ba8589382ee
maximedrouault <maximedrouault@gmail.com>
2025-04-08 09:18:37 +0200

    feat: retrieve user ID from JWT token and store it in auth service

--- front/src/app/features/auth/services/auth.service.ts
+++ front/src/app/features/auth/services/auth.service.ts
@@ -1,21 +1,39 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthToken} from '../interfaces/responses/auth-token';
 import {LoginPayload} from '../interfaces/requests/login-payload.interface';
 import {Observable, tap} from 'rxjs';
 import {environment} from '../../../../environments/environment';
+import {jwtDecode} from 'jwt-decode';
+import {CustomJwtPayload} from '../interfaces/responses/custom-jwt-payload';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  userId: number | undefined = undefined;
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {
+    this.userId = this.getUserIdFromToken();
+  }
 
 
   public getAuthToken(loginPayload: LoginPayload): Observable<AuthToken> {
     return this.http.post<AuthToken>(`${environment.apiUrl}/auth/login`, loginPayload).pipe(
-      tap(response => localStorage.setItem('token', response.token))
+      tap(response => {
+        localStorage.setItem('token', response.token);
+        this.userId = this.getUserIdFromToken()
+      })
     );
+  }
+
+  public getUserIdFromToken(): number | undefined {
+    const token: string | null = localStorage.getItem('token');
+
+    if (token) {
+      return jwtDecode<CustomJwtPayload>(token).userId;
+    } else {
+      return undefined;
+    }
   }
 }
