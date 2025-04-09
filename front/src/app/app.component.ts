cd8b704da80008006c9ee5c16bf44ed9f6fbb35a
maximedrouault <maximedrouault@gmail.com>
2025-04-09 13:46:40 +0200

    feat: refactor header rendering logic to conditionally display based on current route

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {Router, RouterOutlet} from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './layouts/shared/header/header.component';
 import {registerLocaleData} from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
@@ -13,14 +13,7 @@ import localeFr from '@angular/common/locales/fr';
 export class AppComponent {
   title = 'mdd-front';
 
-  constructor(private readonly router: Router) {
+  constructor() {
     registerLocaleData(localeFr)
-  }
-
-
-  shouldShowHeader(): boolean {
-    const excludedRoutes: string[] = ['/login-choice', '/user-login', '/user-register'];
-
-    return !excludedRoutes.includes(this.router.url);
   }
 }
