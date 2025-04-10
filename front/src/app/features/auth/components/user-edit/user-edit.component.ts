f1f90729ad1ba217c5ad899cf8a544ea82cff083
maximedrouault <maximedrouault@gmail.com>
2025-04-10 11:08:14 +0200

    feat: implement user edit form with validation and integrate into user profile

---
+++ front/src/app/features/auth/components/user-edit/user-edit.component.ts
@@ -0,0 +1,36 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {passwordComplexityValidator} from '../../validators/password-complexity.validator';
+import {InputText} from 'primeng/inputtext';
+import {Button} from 'primeng/button';
+
+@Component({
+  selector: 'app-user-edit',
+  imports: [
+    InputText,
+    ReactiveFormsModule,
+    Button
+  ],
+  templateUrl: './user-edit.component.html',
+  styleUrl: './user-edit.component.scss'
+})
+export class UserEditComponent implements OnInit {
+
+  @Input() userId!: number;
+  userEditForm!: FormGroup;
+
+  constructor(private readonly formBuilder: FormBuilder) {}
+
+
+  ngOnInit(): void {
+    this.userEditForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.maxLength(100)]],
+      email: ['', [Validators.required, Validators.email, Validators.maxLength(100)]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(100), passwordComplexityValidator()]],
+    });
+  }
+
+  onUpdate(): void {
+    console.log(this.userEditForm.value);
+  }
+}
