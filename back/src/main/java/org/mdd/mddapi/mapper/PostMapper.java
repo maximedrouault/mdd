7bcbe331872d390ac9fae208dc1f4716911f2225
maximedrouault <maximedrouault@gmail.com>
2025-04-16 12:02:16 +0200

    feat: add Javadoc comments to mapper interfaces for improved documentation and clarity

--- back/src/main/java/org/mdd/mddapi/mapper/PostMapper.java
+++ back/src/main/java/org/mdd/mddapi/mapper/PostMapper.java
@@ -8,23 +8,46 @@ import org.mdd.mddapi.entity.Post;
 
 import java.util.Set;
 
+/**
+ * Mapper interface for converting between Post entities and their corresponding DTOs.
+ * Utilizes MapStruct for automatic mapping.
+ */
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PostMapper {
 
-    // Handle the conversion between PostDto and Post entity
+    /**
+     * Converts a Post entity to a SubscribedPostDto.
+     *
+     * @param post the Post entity to convert
+     * @return the converted SubscribedPostDto
+     */
     @Mapping(source = "author.username", target = "authorName")
     SubscribedPostDto toSubscribedPostDto(Post post);
 
+    /**
+     * Converts a set of Post entities to a set of SubscribedPostDto.
+     *
+     * @param posts the set of Post entities to convert
+     * @return the converted set of SubscribedPostDto
+     */
     Set<SubscribedPostDto> toSubscribedPostDto(Set<Post> posts);
 
-
-    // Handle the conversion between PostDetailsDto and Post entity
+    /**
+     * Converts a Post entity to a PostDetailsDto.
+     *
+     * @param post the Post entity to convert
+     * @return the converted PostDetailsDto
+     */
     @Mapping(source = "topic.name", target = "topicName")
     @Mapping(source = "author.username", target = "authorName")
     PostDetailsDto toPostDetailsDto(Post post);
 
-
-    // Handle the conversion between PostPayloadDto and Post entity
+    /**
+     * Converts a PostPayloadDto to a Post entity.
+     *
+     * @param postPayloadDto the PostPayloadDto to convert
+     * @return the converted Post entity
+     */
     @Mapping(source = "topicId", target = "topic.id")
     Post toEntity(PostPayloadDto postPayloadDto);
 }
