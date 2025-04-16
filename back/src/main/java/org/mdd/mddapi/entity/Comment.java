244b356846537f15052e61c35ad3fc116e858302
maximedrouault <maximedrouault@gmail.com>
2025-04-16 11:50:25 +0200

    feat: add Javadoc comments to Comment, Post, Topic, and User classes for improved documentation and clarity

--- back/src/main/java/org/mdd/mddapi/entity/Comment.java
+++ back/src/main/java/org/mdd/mddapi/entity/Comment.java
@@ -8,6 +8,10 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a comment entity in the system.
+ * A comment is associated with a user (author) and a post.
+ */
 @Getter
 @Setter
 @Entity
@@ -19,22 +23,40 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Comment {
 
+    /**
+     * Unique identifier for the comment.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
+    /**
+     * The date and time when the comment was created.
+     * This field is automatically populated and cannot be updated.
+     */
     @Column(nullable = false, updatable = false)
     @CreationTimestamp
     private LocalDateTime creationDate;
 
+    /**
+     * The content of the comment.
+     */
     @Column(nullable = false)
     private String content;
 
+    /**
+     * The user who authored the comment.
+     * This is a mandatory relationship.
+     */
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, optional = false)
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
+    /**
+     * The post to which the comment belongs.
+     * This is a mandatory relationship.
+     */
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, optional = false)
     @JoinColumn(name = "post_id", nullable = false)
     private Post post;
--- back/src/main/java/org/mdd/mddapi/entity/Post.java
+++ back/src/main/java/org/mdd/mddapi/entity/Post.java
@@ -10,6 +10,10 @@ import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+/**
+ * Represents a post entity in the application.
+ * A post is created by an author, belongs to a topic, and can have multiple comments.
+ */
 @Getter
 @Setter
 @Entity
@@ -21,30 +25,51 @@ import java.util.Set;
 @AllArgsConstructor
 public class Post {
 
+    /**
+     * The unique identifier of the post.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
+    /**
+     * The creation date of the post. This field is automatically set when the post is created.
+     */
     @Column(nullable = false, updatable = false)
     @CreationTimestamp
     private LocalDateTime creationDate;
 
+    /**
+     * The title of the post. It must not exceed 100 characters.
+     */
     @Column(nullable = false, length = 100)
     private String title;
 
+    /**
+     * The content of the post. Stored as a large object (TEXT).
+     */
     @Lob
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
+    /**
+     * The author of the post. This is a mandatory field.
+     */
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, optional = false)
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
+    /**
+     * The topic to which the post belongs. This is a mandatory field.
+     */
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, optional = false)
     @JoinColumn(name = "topic_id", nullable = false)
     private Topic topic;
 
+    /**
+     * The set of comments associated with the post. Comments are removed if the post is deleted.
+     */
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Comment> comments = new LinkedHashSet<>();
 
