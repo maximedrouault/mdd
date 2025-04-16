a711fd596caf13e582a0291a5c36719c49f61701
maximedrouault <maximedrouault@gmail.com>
2025-04-16 12:17:11 +0200

    feat: add Javadoc comments to CustomUserDetailsService, JwtService, and SecurityConfig classes for improved documentation and clarity

--- back/src/main/java/org/mdd/mddapi/service/JwtService.java
+++ back/src/main/java/org/mdd/mddapi/service/JwtService.java
@@ -16,6 +16,9 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * Service for generating JSON Web Tokens (JWT) for authenticated users.
+ */
 @Service
 @RequiredArgsConstructor
 public class JwtService {
@@ -26,7 +29,12 @@ public class JwtService {
     private final JwtEncoder jwtEncoder;
     private final UserRepository userRepository;
 
-
+    /**
+     * Generates a JWT for the given authentication object.
+     *
+     * @param authentication the authentication object containing user details
+     * @return the generated JWT as a string
+     */
     public String generateToken(Authentication authentication) {
         Instant now = Instant.now();
         Instant expiration = now.plus(expirationTime, ChronoUnit.SECONDS);
@@ -44,6 +52,13 @@ public class JwtService {
         return jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
     }
 
+    /**
+     * Retrieves the user ID based on the provided email or username.
+     *
+     * @param emailOrUsername the email or username of the user
+     * @return the user ID
+     * @throws UsernameNotFoundException if the user is not found
+     */
     private Long getUserId(String emailOrUsername) {
         User foundUser = userRepository.findByEmailOrUsername(emailOrUsername, emailOrUsername)
                 .orElseThrow(() -> new UsernameNotFoundException(emailOrUsername));
